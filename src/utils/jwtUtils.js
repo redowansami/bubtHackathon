3f95f0a880b7753ddb17ad5c0482412e0d20bebb
Redowan Billah Sami <bsse1409@iit.du.ac.bd>
2025-11-20 17:21:49 +0600

    feat: Add user and auth features

---
+++ src/utils/jwtUtils.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRY || '7d',
+    }
+  );
+};
+
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
